3da75d1f7283f36a1f155fbd85841d80bdac57db
Robert Berge <31069197+ZenitH-AT@users.noreply.github.com>
2018-09-26 22:59:54 +0200

    Completed admin ticket interaction
    It's not the best solution, but it works... Combining it into a function was not easy so I gave up.

--- learningspacefinal/AccSystem/resource/templates/back/ticket.php
+++ learningspacefinal/AccSystem/resource/templates/back/ticket.php
@@ -29,12 +29,76 @@
                     <td><?php echo $row['ticketCategory'] ?></td> 
                     <td><?php echo $row['ticketTime'] ?></td>
 
-                    <td><button type="button" class="btn btn-success formbutton" data-toggle="modal" data-target="#openTicketModal<?php echo $row['ticketID']; ?>"><span class="fa fa-comments" style="color:white"></button></form></td>
-                    <td><form method="POST"><button class="btn btn-danger formbutton" name="closereopen<?php echo $row['ticketID']; ?>" onclick="return confirm('Are you sure you want to close <?php echo $row['ticketSubject'] ?>?')"><span class="fa fa-times" style="color:white"></button></form></td>
+                    <td><button type="button" class="btn btn-success formbutton" data-toggle="modal" data-target="#ticketPopup<?php echo $row['ticketID']; ?>"><span class="fa fa-comments" style="color:white"></button></form></td>
+                    <td><form method="post"><button class="btn btn-danger formbutton" name="closereopen<?php echo $row['ticketID']; ?>" onclick="return confirm('Are you sure you want to close <?php echo $row['ticketSubject'] ?>?')"><span class="fa fa-times" style="color:white"></button></form></td>
                 </tr> 
-                <?php
-                //Close/reopen button handling
-                close_open_ticket($row['ticketID']);
+                
+                <!-- Conversation modal -->
+                <div class="modal fade" id="ticketPopup<?php echo $row['ticketID']; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered modal-md" role="document">
+                        <div class="modal-content">  
+                            <div class="modal-header">
+                                <h5><?php echo $row['ticketSubject']; ?></h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close"></button>
+                            </div> 
+                            <div class="modal-body">
+                                <!-- Reply to ticket form -->
+                                <form action="" method="post">
+                                    <textarea type="text" class="form-control" name="messageText<?php echo $row['ticketID']; ?>" placeholder="Type your message here" rows="4" required></textarea><br />
+                                    <button type="submit" class="btn btn-outline-success formbutton" style="float:right" name="messageSend<?php echo $row['ticketID']; ?>">Reply to ticket</button>
+                                </form> <?php
+
+                                //Reply to ticket button code
+                                if(isset($_POST['messageSend' . $row['ticketID']])){
+                                    query("INSERT INTO helpticketmessage (ticketID, adminID, messageText, messageTime) VALUES('{$row['ticketID']}', '{$_SESSION["idadmin"]}', '{$_POST['messageText' . $row['ticketID']]}', now())");
+                                    ?><script>alert("Your reply has been sent.");</script><?php
+                                } ?>
+
+                                <br /><br /><h6>Conversation</h6><?php
+                                
+                                //Data for while loop
+                                $resultmessages = query("SELECT studID, adminID, messageText, messageTime FROM helpticketmessage WHERE ticketID = " . $row['ticketID'] . " ORDER BY messageTime DESC");
+                                $message_count = $resultmessages->num_rows;
+
+                                //while loop to create cards for each message
+                                while ($message = $resultmessages->fetch_assoc()) { 
+                                    //Getting message author type; either student or admin (the message author is based on which field in the message record is null)
+                                    $messageauthortype = empty($message['adminID']) ? 'student' : 'admin'; 
+                                    
+                                    //Getting message author name
+                                    $messageauthorname;
+
+                                    if ($messageauthortype == "student") {
+                                        $sqlstudentauthor = query("SELECT studFirstName, studLastName FROM student WHERE studID = " . $message['studID']);
+                                        $studentname = mysqli_fetch_assoc($sqlstudentauthor);
+                                        $messageauthorname = $studentname['studFirstName'] . ' ' . $studentname['studLastName'];
+                                    } else {
+                                        $sqladminauthor = query("SELECT adminFirstN, adminLastN FROM admin WHERE adminID = " . $message['adminID']);
+                                        $adminname = mysqli_fetch_assoc($sqladminauthor);
+                                        $messageauthorname = $adminname['adminFirstN'] . ' ' . $adminname['adminLastN'];                                          
+                                    } ?>
+
+                                    <div class="card">
+                                        <div class="card-header text-secondary">
+                                            <span style="float:left"><?php echo $messageauthorname; ?></span><span style="float:right"><?php echo $message['messageTime']; ?></span>
+                                        </div>
+                                        <div class="card-body">
+                                            <p class="card-text">
+                                                <span class="text-secondary"><?php echo $message['messageText']; ?></span>
+                                            </p>
+                                        </div> 
+                                    </div><br /> <?php
+                                } ?>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div><?php
+
+                //Close button handling
+                close_open_ticket($row['ticketID'], $row['isActive']);
             } ?>
         </tbody>
     </table>
@@ -67,15 +131,70 @@
                     <td><?php echo $row['ticketCategory'] ?></td> 
                     <td><?php echo $row['ticketTime'] ?></td>
 
-                    <td><button type="button" class="btn btn-success formbutton" data-toggle="modal" data-target="#closedTicketModal<?php echo $row['ticketID']; ?>"><span class="fa fa-comments" style="color:white"></button></form></td>
-                    <td><form method="POST"><button class="btn btn-info formbutton" name="closereopen<?php echo $row['ticketID']; ?>" onclick="return confirm('Are you sure you want to reopen <?php echo $row['ticketSubject'] ?>?')"><span class="fa fa-arrow-up" style="color:white"></button></form></td>
+                    <td><button type="button" class="btn btn-success formbutton" data-toggle="modal" data-target="#ticketPopup<?php echo $row['ticketID']; ?>"><span class="fa fa-comments" style="color:white"></button></form></td>
+                    <td><form method="post"><button class="btn btn-info formbutton" name="closereopen<?php echo $row['ticketID']; ?>" onclick="return confirm('Are you sure you want to reopen <?php echo $row['ticketSubject'] ?>?')"><span class="fa fa-arrow-up" style="color:white"></button></form></td>
                 </tr> 
-                <?php 
-                //Close/reopen button handling
-                close_open_ticket($row['ticketID']);
+                
+                <!-- Conversation modal -->
+                <div class="modal fade" id="ticketPopup<?php echo $row['ticketID']; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered modal-md" role="document">
+                        <div class="modal-content">  
+                            <div class="modal-header">
+                                <h5><?php echo $row['ticketSubject']; ?></h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close"></button>
+                            </div> 
+                            <div class="modal-body">
+                                <div class='alert alert-info fade show text-center' role='alert'>
+                                    You cannot reply to a closed ticket.
+                                </div> 
+
+                                <h6>Conversation</h6><?php
+                                
+                                //Data for while loop
+                                $resultmessages = query("SELECT studID, adminID, messageText, messageTime FROM helpticketmessage WHERE ticketID = " . $row['ticketID'] . " ORDER BY messageTime DESC");
+                                $message_count = $resultmessages->num_rows;
+
+                                //while loop to create cards for each message
+                                while ($message = $resultmessages->fetch_assoc()) { 
+                                    //Getting message author type; either student or admin (the message author is based on which field in the message record is null)
+                                    $messageauthortype = empty($message['adminID']) ? 'student' : 'admin'; 
+                                    
+                                    //Getting message author name
+                                    $messageauthorname;
+
+                                    if ($messageauthortype == "student") {
+                                        $sqlstudentauthor = query("SELECT studFirstName, studLastName FROM student WHERE studID = " . $message['studID']);
+                                        $studentname = mysqli_fetch_assoc($sqlstudentauthor);
+                                        $messageauthorname = $studentname['studFirstName'] . ' ' . $studentname['studLastName'];
+                                    } else {
+                                        $sqladminauthor = query("SELECT adminFirstN, adminLastN FROM admin WHERE adminID = " . $message['adminID']);
+                                        $adminname = mysqli_fetch_assoc($sqladminauthor);
+                                        $messageauthorname = $adminname['adminFirstN'] . ' ' . $adminname['adminLastN'];                                          
+                                    } ?>
+
+                                    <div class="card">
+                                        <div class="card-header text-secondary">
+                                            <span style="float:left"><?php echo $messageauthorname; ?></span><span style="float:right"><?php echo $message['messageTime']; ?></span>
+                                        </div>
+                                        <div class="card-body">
+                                            <p class="card-text">
+                                                <span class="text-secondary"><?php echo $message['messageText']; ?></span>
+                                            </p>
+                                        </div> 
+                                    </div><br /> <?php
+                                } ?>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div><?php 
+                
+                //Reopen button handling
+                close_open_ticket($row['ticketID'], $row['isActive']);
             } ?>
         </tbody>
     </table>
 </div>
-
 
