41c2badfccc2d2151663fbddf1fded14c3cae170
Robert Berge <31069197+ZenitH-AT@users.noreply.github.com>
2018-09-26 19:20:04 +0200

    Admins can close and reopen tickets

--- learningspacefinal/AccSystem/resource/templates/back/ticket.php
+++ learningspacefinal/AccSystem/resource/templates/back/ticket.php
@@ -1,22 +1,81 @@
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h2">Tickets</h1>
+</div>
 
-    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Tickets</h1>
-        <div class="btn-toolbar mb-2 mb-md-0">
-            Some
-<!--            <div class="btn-group mr-2">
-                <button class="btn btn-sm btn-outline-secondary">Share</button>
-                <button class="btn btn-sm btn-outline-secondary">Export</button>
-            </div>
-            <button class="btn btn-sm btn-outline-secondary dropdown-toggle">
-                <span data-feather="calendar"></span>
-                This week
-            </button>-->
-        </div>
-    </div>
+<h5>Open tickets</h5>
+<div class="table-responsive">
+    <table class="table table-striped table-sm">
+        <thead>
+            <tr>  
+                <th>Ticket ID</th>
+                <th>Student ID</th>
+                <th>Subject</th>
+                <th>Category</th>
+                <th>Date created</th>
 
-    <!--<canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>-->
+                <th>View conversation</th>
+                <th>Close ticket</th>
+            </tr>
+        </thead>
+        <tbody> <?php
+            $query = query("SELECT * FROM helpticket WHERE isActive = 1 ORDER BY ticketID DESC");
+            confirm($query);
 
-    <h2>Section title</h2>
-    <div class="table-responsive">
+            while ($row = fetch_array($query)) { ?>
+                <tr>
+                    <td><?php echo $row['ticketID'] ?></td>
+                    <td><?php echo $row['studID'] ?></td>
+                    <td><?php echo $row['ticketSubject'] ?></td>
+                    <td><?php echo $row['ticketCategory'] ?></td> 
+                    <td><?php echo $row['ticketTime'] ?></td>
 
-    </div>
+                    <td><button type="button" class="btn btn-success formbutton" data-toggle="modal" data-target="#openTicketModal<?php echo $row['ticketID']; ?>"><span class="fa fa-comments" style="color:white"></button></form></td>
+                    <td><form method="POST"><button class="btn btn-danger formbutton" name="closereopen<?php echo $row['ticketID']; ?>"><span class="fa fa-times" style="color:white"></button></form></td>
+                </tr> 
+                <?php
+                //Close/reopen button handling
+                close_open_ticket($row['ticketID']);
+            } ?>
+        </tbody>
+    </table>
+</div><br />
+
+<h5>Closed tickets</h5>
+<div class="table-responsive">
+    <table class="table table-striped table-sm">
+        <thead>
+            <tr>  
+                <th>Ticket ID</th>
+                <th>Student ID</th>
+                <th>Subject</th>
+                <th>Category</th>
+                <th>Date created</th>
+
+                <th>View conversation</th>
+                <th>Reopen ticket</th>
+            </tr>
+        </thead>
+        <tbody> <?php
+            $query = query("SELECT * FROM helpticket WHERE isActive = 0 ORDER BY ticketID DESC");
+            confirm($query);
+
+            while ($row = fetch_array($query)) { ?>
+                <tr>
+                    <td><?php echo $row['ticketID'] ?></td>
+                    <td><?php echo $row['studID'] ?></td>
+                    <td><?php echo $row['ticketSubject'] ?></td>
+                    <td><?php echo $row['ticketCategory'] ?></td> 
+                    <td><?php echo $row['ticketTime'] ?></td>
+
+                    <td><button type="button" class="btn btn-success formbutton" data-toggle="modal" data-target="#closedTicketModal<?php echo $row['ticketID']; ?>"><span class="fa fa-comments" style="color:white"></button></form></td>
+                    <td><form method="POST"><button class="btn btn-info formbutton" name="closereopen<?php echo $row['ticketID']; ?>"><span class="fa fa-arrow-up" style="color:white"></button></form></td>
+                </tr> 
+                <?php 
+                //Close/reopen button handling
+                close_open_ticket($row['ticketID']);
+            } ?>
+        </tbody>
+    </table>
+</div>
+
+
