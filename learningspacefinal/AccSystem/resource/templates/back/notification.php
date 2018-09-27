57cf484fbee0a99a36456f8e8dabfba2f250907c
Robert Berge <31069197+ZenitH-AT@users.noreply.github.com>
2018-09-27 21:30:11 +0200

    Completed admin notification creation
    Various buttons in the UI should also make them

--- learningspacefinal/AccSystem/resource/templates/back/notification.php
+++ learningspacefinal/AccSystem/resource/templates/back/notification.php
@@ -1,146 +1,111 @@
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h2">Notifications</h1>
+</div>
 
-    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Notifications</h1>
-        <div class="btn-toolbar mb-2 mb-md-0">
-            Some
-<!--            <div class="btn-group mr-2">
-                <button class="btn btn-sm btn-outline-secondary">Share</button>
-                <button class="btn btn-sm btn-outline-secondary">Export</button>
-            </div>
-            <button class="btn btn-sm btn-outline-secondary dropdown-toggle">
-                <span data-feather="calendar"></span>
-                This week
-            </button>-->
+<h5>Create notification</h5><br />
+<div class="container">
+    <div class="row">
+        <div class="col-md-10">
+            <form method="post">
+                <div class="form-group row">
+                    <div class="col-sm-6">
+                        <input type="text" class="form-control" id="title" name="title" placeholder="Title" required>
+                    </div>
+                    <div class="col-sm-6">
+                        <select class="combobox form-control text-muted" id="type" name="type" required>
+                            <option value="default" selected="selected">Select type (colour)</option>
+                            <option value="default">Default</option>
+                            <option value="info" class="text-info">Info</option>
+                            <option value="success" class="text-success">Success</option>
+                            <option value="warning" class="text-warning">Warning</option>
+                            <option value="danger" class="text-danger">Danger</option>
+                            <option value="notice" class="bg-warning text-dark">Notice</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-sm-6">
+                        <textarea class="form-control" id="body" name="body" placeholder="Message" rows="7" required></textarea>
+                    </div>
+                    <div class="col-sm-6">
+                        <input type="text" class="form-control" id="recipients" name="recipients" placeholder="Recipient ID(s)" required><br />
+                        <label class="form-text text-muted">Separate multiple student IDs with a comma.<br/>Type * to send to all students.</label>
+                        
+                        <button type="submit" class="btn btn-outline-success formbutton" style="right:0; position:absolute; bottom:0;" name="send">Send notification</button><?php
+
+                        //Send button handling
+                        if (isset($_POST['send'])) {
+                            $title = escape_String($_POST['title']);
+                            $body = escape_String($_POST['body']);
+                            $type = escape_String($_POST['type']);
+                            $inputids = escape_String($_POST['recipients']);
+
+                            send_notification($title, $body, $type, $inputids);
+                        } ?>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
+</div>
 
-    <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
+<br /><h5>Sent notifications</h5>
+<div class="table-responsive">
+    <table class="table table-striped table-sm">
+        <thead>
+            <tr>  
+                <th>Notification ID</th>
+                <th>Student ID (recipient)</th>
+                <th>Title</th>
+                <th>Message</th>
+                <th>Type</th>
+                <th>Date sent</th>
+                <th>Status</th>
 
-    <h2>Section title</h2>
-    <div class="table-responsive">
-        <table class="table table-striped table-sm">
-            <thead>
+                <th>Delete</th>
+            </tr>
+        </thead>
+        <tbody> <?php
+            $query = query("SELECT * FROM notification ORDER BY notificationID DESC");
+            confirm($query);
+
+            while ($row = fetch_array($query)) { 
+                //Setting notification type colour
+                $typeclass ='';
+
+                if($row['type'] == 'info') {
+                    $typeclass = 'class="text-info"';
+                } else if ($row['type'] == 'success') {
+                    $typeclass = 'class="text-success"';
+                } else if ($row['type'] == 'warning') {
+                    $typeclass = 'class="text-warning"';
+                } else if ($row['type'] == 'danger') {
+                    $typeclass = 'class="text-danger"';
+                } else if ($row['type'] == 'notice') {
+                    $typeclass = 'class="bg-warning text-dark"';
+                } ?>
+
                 <tr>
-                    <th>#</th>
-                    <th>Header</th>
-                    <th>Header</th>
-                    <th>Header</th>
-                    <th>Header</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1,001</td>
-                    <td>Lorem</td>
-                    <td>ipsum</td>
-                    <td>dolor</td>
-                    <td>sit</td>
-                </tr>
-                <tr>
-                    <td>1,002</td>
-                    <td>amet</td>
-                    <td>consectetur</td>
-                    <td>adipiscing</td>
-                    <td>elit</td>
-                </tr>
-                <tr>
-                    <td>1,003</td>
-                    <td>Integer</td>
-                    <td>nec</td>
-                    <td>odio</td>
-                    <td>Praesent</td>
-                </tr>
-                <tr>
-                    <td>1,003</td>
-                    <td>libero</td>
-                    <td>Sed</td>
-                    <td>cursus</td>
-                    <td>ante</td>
-                </tr>
-                <tr>
-                    <td>1,004</td>
-                    <td>dapibus</td>
-                    <td>diam</td>
-                    <td>Sed</td>
-                    <td>nisi</td>
-                </tr>
-                <tr>
-                    <td>1,005</td>
-                    <td>Nulla</td>
-                    <td>quis</td>
-                    <td>sem</td>
-                    <td>at</td>
-                </tr>
-                <tr>
-                    <td>1,006</td>
-                    <td>nibh</td>
-                    <td>elementum</td>
-                    <td>imperdiet</td>
-                    <td>Duis</td>
-                </tr>
-                <tr>
-                    <td>1,007</td>
-                    <td>sagittis</td>
-                    <td>ipsum</td>
-                    <td>Praesent</td>
-                    <td>mauris</td>
-                </tr>
-                <tr>
-                    <td>1,008</td>
-                    <td>Fusce</td>
-                    <td>nec</td>
-                    <td>tellus</td>
-                    <td>sed</td>
-                </tr>
-                <tr>
-                    <td>1,009</td>
-                    <td>augue</td>
-                    <td>semper</td>
-                    <td>porta</td>
-                    <td>Mauris</td>
-                </tr>
-                <tr>
-                    <td>1,010</td>
-                    <td>massa</td>
-                    <td>Vestibulum</td>
-                    <td>lacinia</td>
-                    <td>arcu</td>
-                </tr>
-                <tr>
-                    <td>1,011</td>
-                    <td>eget</td>
-                    <td>nulla</td>
-                    <td>Class</td>
-                    <td>aptent</td>
-                </tr>
-                <tr>
-                    <td>1,012</td>
-                    <td>taciti</td>
-                    <td>sociosqu</td>
-                    <td>ad</td>
-                    <td>litora</td>
-                </tr>
-                <tr>
-                    <td>1,013</td>
-                    <td>torquent</td>
-                    <td>per</td>
-                    <td>conubia</td>
-                    <td>nostra</td>
-                </tr>
-                <tr>
-                    <td>1,014</td>
-                    <td>per</td>
-                    <td>inceptos</td>
-                    <td>himenaeos</td>
-                    <td>Curabitur</td>
-                </tr>
-                <tr>
-                    <td>1,015</td>
-                    <td>sodales</td>
-                    <td>ligula</td>
-                    <td>in</td>
-                    <td>libero</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+                    <td><?php echo $row['notificationID'] ?></td>
+                    <td><?php echo $row['studID'] ?></td>
+                    <td><?php echo $row['title'] ?></td>
+                    <td><?php echo $row['body'] ?></td> 
+                    <td><?php echo '<textt ' . $typeclass . '>' . $row['type'] . '</text>' ?></td>
+                    <td><?php echo $row['time'] ?></td>
+                    <td><?php echo $row['status'] == 1 ? '<text class="text-info">read</text>' : 'unread' ?></td>
+
+                    <td><form method="post"><button class="btn btn-danger formbutton" name="delete<?php echo $row['notificationID']; ?>" onclick="return confirm('Are you sure you want to delete <?php echo $row['title'] ?>?')"><span class="fa fa-times" style="color:white"></button></form></td>
+                </tr><?php
+
+                //Delete button handling
+                if (isset($_POST['delete' . $row['notificationID']])) {                  
+                    query("DELETE FROM notification WHERE notificationID = " . $row['notificationID']);
+            
+                    header("Refresh:0");
+                    exit();
+                } 
+            } ?>
+        </tbody>
+        <thead>
+    </table>
+</div>
