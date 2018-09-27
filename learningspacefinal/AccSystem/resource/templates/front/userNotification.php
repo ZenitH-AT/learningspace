3eb8d9566c1bd6046ca1c9d6b818252fd309fb35
Robert Berge <31069197+ZenitH-AT@users.noreply.github.com>
2018-09-28 01:09:28 +0200

    Cleaned up code

--- learningspacefinal/AccSystem/resource/templates/front/userNotification.php
+++ learningspacefinal/AccSystem/resource/templates/front/userNotification.php
@@ -6,12 +6,12 @@
         <legend class="header col-md-10" style="margin-bottom: -2%">Unread notifications</legend><br /><?php
 
         //Determining number of notifications
-        $query = query("SELECT * FROM notification WHERE studID = " . $_SESSION["iduser"] . " AND status = 0 ORDER BY time DESC");
-        confirm($query);
+        $sqlunread = query("SELECT * FROM notification WHERE studID = " . $_SESSION["iduser"] . " AND status = 0 ORDER BY time DESC");
+        confirm($sqlunread);
 
-        $row_count = $query->num_rows;
+        $unreadcount = $sqlunread->num_rows;
         
-        if ($row_count == 0) { ?>
+        if ($unreadcount == 0) { ?>
             <a class="col-sm-6 col-md-6" style="text-muted">No new notifications</a><?php
         } else { ?>
             <legend class="header col-md-10" style="margin-bottom: -0.5%"><form method="post"><button class="btn btn-outline-secondary formbutton" name="markread" onclick="return confirm('Are you sure you want to mark all notifications as read?')">Mark all as read</button></form></legend><?php
@@ -27,28 +27,11 @@
 
         <div class="row"><?php
             //Generating notification cards
-            while ($row = fetch_array($query)) { 
-                //Determining which icon to use based on notification type
-                $iconclass;  
-
-                if ($row['type'] == 'default') {
-                    $iconclass = 'fas fa-comment-dots';
-                } else if ($row['type'] == 'info') {
-                    $iconclass = 'fas fa-info-circle';
-                } else if ($row['type'] == 'success') {
-                    $iconclass = 'fas fa-check-circle';
-                } else if ($row['type'] == 'warning') {
-                    $iconclass = 'fas fa-exclamation-triangle';
-                } else if ($row['type'] == 'danger') {
-                    $iconclass = 'fas fa-times-circle';
-                } else if ($row['type'] == 'notice') {
-                    $iconclass = 'fas fa-flag';
-                } ?>
-
+            while ($row = fetch_array($sqlunread)) { ?>
                 <!-- notification card -->
                 <div class="col-sm-6 col-md-6">
                     <div class="alert-message alert-message-<?php echo $row['type'] ?>">
-                        <i class="<?php echo $iconclass ?> text-muted"></i>
+                        <i class="<?php notification_icon($row['type']) ?> text-muted"></i>
                         <a class="text-muted"><?php echo $row['time'] ?></a>
                         <h4><?php echo $row['title'] ?></h4> 
                         <p><?php echo $row['body'] ?></p>
@@ -61,34 +44,15 @@
 
         <div class="row"><?php
             //Only returns notifications from the last 30 days in order to not flood the user with too much information
-            $query = query("SELECT * FROM notification WHERE studID = " . $_SESSION["iduser"] . " AND status = 1 AND time BETWEEN now() - INTERVAL 30 DAY AND now() ORDER BY time DESC");
-            confirm($query);
-    
-            $row_count = $query->num_rows;
+            $sqlread = query("SELECT * FROM notification WHERE studID = " . $_SESSION["iduser"] . " AND status = 1 AND time BETWEEN now() - INTERVAL 30 DAY AND now() ORDER BY time DESC");
+            confirm($sqlread);
 
             //Generating notification cards
-            while ($row = fetch_array($query)) { 
-                //Determining which icon to use based on notification type
-                $iconclass;  
-
-                if ($row['type'] == 'default') {
-                    $iconclass = 'fas fa-comment-dots';
-                } else if ($row['type'] == 'info') {
-                    $iconclass = 'fas fa-info-circle';
-                } else if ($row['type'] == 'success') {
-                    $iconclass = 'fas fa-check-circle';
-                } else if ($row['type'] == 'warning') {
-                    $iconclass = 'fas fa-exclamation-triangle';
-                } else if ($row['type'] == 'danger') {
-                    $iconclass = 'fas fa-times-circle';
-                } else if ($row['type'] == 'notice') {
-                    $iconclass = 'fas fa-flag';
-                } ?>
-
+            while ($row = fetch_array($sqlread)) { ?>
                 <!-- notification card -->
                 <div class="col-sm-6 col-md-6">
                     <div class="alert-message alert-message-<?php echo $row['type'] ?>">
-                        <i class="<?php echo $iconclass ?> text-muted"></i>
+                        <i class="<?php notification_icon($row['type']) ?> text-muted"></i>
                         <a class="text-muted"><?php echo $row['time'] ?></a>
                         <h4><?php echo $row['title'] ?></h4> 
                         <p><?php echo $row['body'] ?></p>
