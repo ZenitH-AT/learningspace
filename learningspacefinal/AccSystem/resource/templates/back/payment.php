8ee75647b124e9d099926b51c3ae62c009b43cc7
Robert Berge <31069197+ZenitH-AT@users.noreply.github.com>
2018-09-30 03:53:46 +0200

    Viewing payments on admin side

--- learningspacefinal/AccSystem/resource/templates/back/payment.php
+++ learningspacefinal/AccSystem/resource/templates/back/payment.php
@@ -1,146 +1,43 @@
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h2">Payments</h1>
+</div>
 
-    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Payments</h1>
-        <div class="btn-toolbar mb-2 mb-md-0">
-            Some
-<!--            <div class="btn-group mr-2">
-                <button class="btn btn-sm btn-outline-secondary">Share</button>
-                <button class="btn btn-sm btn-outline-secondary">Export</button>
-            </div>
-            <button class="btn btn-sm btn-outline-secondary dropdown-toggle">
-                <span data-feather="calendar"></span>
-                This week
-            </button>-->
-        </div>
-    </div>
-
-    <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
-
-    <h2>Section title</h2>
+<h5>Payment records</h5>
+<div class="table-responsive">
     <div class="table-responsive">
         <table class="table table-striped table-sm">
             <thead>
                 <tr>
-                    <th>#</th>
-                    <th>Header</th>
-                    <th>Header</th>
-                    <th>Header</th>
-                    <th>Header</th>
+                    <th>Payment ID</th>
+                    <th>Card No</th>
+                    <th>Card month</th>
+                    <th>Card year</th>
+                    <th>Payment amount</th>
+                    <th>Payment month</th>
+                    <th>Student ID</th>
+                    <th>Room ID</th>
+                    <th>Payment status</th>
+                    <th>Payment date</th>
                 </tr>
             </thead>
-            <tbody>
-                <tr>
-                    <td>1,001</td>
-                    <td>Lorem</td>
-                    <td>ipsum</td>
-                    <td>dolor</td>
-                    <td>sit</td>
-                </tr>
-                <tr>
-                    <td>1,002</td>
-                    <td>amet</td>
-                    <td>consectetur</td>
-                    <td>adipiscing</td>
-                    <td>elit</td>
-                </tr>
-                <tr>
-                    <td>1,003</td>
-                    <td>Integer</td>
-                    <td>nec</td>
-                    <td>odio</td>
-                    <td>Praesent</td>
-                </tr>
-                <tr>
-                    <td>1,003</td>
-                    <td>libero</td>
-                    <td>Sed</td>
-                    <td>cursus</td>
-                    <td>ante</td>
-                </tr>
-                <tr>
-                    <td>1,004</td>
-                    <td>dapibus</td>
-                    <td>diam</td>
-                    <td>Sed</td>
-                    <td>nisi</td>
-                </tr>
-                <tr>
-                    <td>1,005</td>
-                    <td>Nulla</td>
-                    <td>quis</td>
-                    <td>sem</td>
-                    <td>at</td>
-                </tr>
-                <tr>
-                    <td>1,006</td>
-                    <td>nibh</td>
-                    <td>elementum</td>
-                    <td>imperdiet</td>
-                    <td>Duis</td>
-                </tr>
-                <tr>
-                    <td>1,007</td>
-                    <td>sagittis</td>
-                    <td>ipsum</td>
-                    <td>Praesent</td>
-                    <td>mauris</td>
-                </tr>
-                <tr>
-                    <td>1,008</td>
-                    <td>Fusce</td>
-                    <td>nec</td>
-                    <td>tellus</td>
-                    <td>sed</td>
-                </tr>
-                <tr>
-                    <td>1,009</td>
-                    <td>augue</td>
-                    <td>semper</td>
-                    <td>porta</td>
-                    <td>Mauris</td>
-                </tr>
-                <tr>
-                    <td>1,010</td>
-                    <td>massa</td>
-                    <td>Vestibulum</td>
-                    <td>lacinia</td>
-                    <td>arcu</td>
-                </tr>
-                <tr>
-                    <td>1,011</td>
-                    <td>eget</td>
-                    <td>nulla</td>
-                    <td>Class</td>
-                    <td>aptent</td>
-                </tr>
-                <tr>
-                    <td>1,012</td>
-                    <td>taciti</td>
-                    <td>sociosqu</td>
-                    <td>ad</td>
-                    <td>litora</td>
-                </tr>
-                <tr>
-                    <td>1,013</td>
-                    <td>torquent</td>
-                    <td>per</td>
-                    <td>conubia</td>
-                    <td>nostra</td>
-                </tr>
-                <tr>
-                    <td>1,014</td>
-                    <td>per</td>
-                    <td>inceptos</td>
-                    <td>himenaeos</td>
-                    <td>Curabitur</td>
-                </tr>
-                <tr>
-                    <td>1,015</td>
-                    <td>sodales</td>
-                    <td>ligula</td>
-                    <td>in</td>
-                    <td>libero</td>
-                </tr>
+            <tbody> <?php
+                $query = query("SELECT * FROM payment ORDER BY payID DESC");
+                confirm($query);
+    
+                while ($row = fetch_array($query)) { ?>
+                    <tr>
+                        <td><?php echo $row['payID'] ?></td>
+                        <td><?php echo $row['cardNumber'] ?></td>
+                        <td><?php echo $row['cardMonth'] ?></td>
+                        <td><?php echo $row['cardYear'] ?></td> 
+                        <td><?php echo $row['payAmount'] ?></td>
+                        <td><?php echo $row['payMonth'] ?></td>
+                        <td><?php echo $row['studID'] ?></td>
+                        <td><?php echo $row['roomID'] ?></td>
+                        <td><?php echo ($row['paymentStatus'] == 1 ? '<text class="text-success">paid</text>' : '<text class="text-danger">unpaid</text>') ?></td>
+                        <td><?php echo $row['paymentDate'] ?></td>
+                    </tr> <?php
+                } ?>
             </tbody>
         </table>
     </div>
