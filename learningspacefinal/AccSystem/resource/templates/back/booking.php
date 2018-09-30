22923f9634811dfe0ed74f4deb2b410a528e484c
Wenykeny <31759520+Wenykeny@users.noreply.github.com>
2018-09-30 20:57:40 +0200

    I've Changed
    booking, room and viewing

--- learningspacefinal/AccSystem/resource/templates/back/booking.php
+++ learningspacefinal/AccSystem/resource/templates/back/booking.php
@@ -1,112 +1,48 @@
-
-    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Bookings</h1>
-        <div class="btn-toolbar mb-2 mb-md-0">
-            Some
-<!--            <div class="btn-group mr-2">
-                <button class="btn btn-sm btn-outline-secondary">Share</button>
-                <button class="btn btn-sm btn-outline-secondary">Export</button>
-            </div>
-            <button class="btn btn-sm btn-outline-secondary dropdown-toggle">
-                <span data-feather="calendar"></span>
-                This week
-            </button>-->
-        </div>
-    </div>
-
-    <!--<canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>-->
-
-    <h2>Section title</h2>
-    <div class="table-responsive">
+    
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="page-header">Bookings</h1>
+    <div class="btn-toolbar mb-2 mb-md-0">
+        <?php if(isset($_GET['student'])){ 
+            if($_GET['student']=="deleted"){ ?>
+        <div class="alert alert-success alert-dismissible fade show text-center" role="alert">
+                <strong>Success!</strong> A student record was removed.
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button></div>
+            <?php }}?>
         
     </div>
+</div>
 
-            <div id="wrapper">
-
-            <div id="page-wrapper">
-
-                <div class="container-fluid">
-                    <div class="row">
-
-                        <div class="col-lg-4 col-md-6">
-                            <div class="panel panel-yellow">
-                                <div class="panel-heading">
-                                    <div class="row">
-                                        <div class="col-xs-3">
-                                            <i class="fa fa-shopping-cart fa-5x"></i>
-                                        </div>
-                                        <div class="col-xs-9 text-right">
-                                            <div class="huge">124</div>
-                                            <div>New Orders!</div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <a href="#">
-                                    <div class="panel-footer">
-                                        <span class="pull-left">View Details</span>
-                                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                        <div class="clearfix"></div>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-
-
-                        <div class="col-lg-4 col-md-6">
-                            <div class="panel panel-red">
-                                <div class="panel-heading">
-                                    <div class="row">
-                                        <div class="col-xs-3">
-                                            <i class="fa fa-support fa-5x"></i>
-                                        </div>
-                                        <div class="col-xs-9 text-right">
-                                            <div class="huge">13</div>
-                                            <div>Products!</div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <a href="#">
-                                    <div class="panel-footer">
-                                        <span class="pull-left">View Details</span>
-                                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                        <div class="clearfix"></div>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-4 col-md-6">
-                            <div class="panel panel-green">
-                                <div class="panel-heading">
-                                    <div class="row">
-                                        <div class="col-xs-3">
-                                            <i class="fa fa-tasks fa-5x"></i>
-                                        </div>
-                                        <div class="col-xs-9 text-right">
-                                            <div class="huge">12</div>
-                                            <div>Categories!</div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <a href="#">
-                                    <div class="panel-footer">
-                                        <span class="pull-left">View Details</span>
-                                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                        <div class="clearfix"></div>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-
-
-                </div>
-                <!-- /.row -->
-
-            </div>
-            <!-- /.container-fluid -->
-
-        </div>
-        <!-- /#page-wrapper -->
-
+<div class="row">
+    <div class="col-lg-12">
+        <ol class="breadcrumb text-secondary ">
+            <li class="breadcrumb-item text-secondary"><i class="fa fa-book-open"></i> Bookings</li>  
+        </ol>
     </div>
-    <!-- /#wrapper -->
+</div>
+<!--</nav>-->
+
+<div class="table-responsive">
+    <table class="table table-striped table-sm">
+        <thead>
+            <tr>  
+                <th>ID</th>
+                <th>Student_ID</th>
+                <th>Room_ID</th>
+                <th>Started_Date</th>
+                <th>Final_Date</th>
+                <th>Staying_Period</th>
+                <th>booking_Date</th>
+                <th>booking_Status</th>
+
+                <th>Pending</th>
+                <th>Edit</th>
+                <th>Remove</th>
+            </tr>
+        </thead>
+        <tbody class="text-center">
+            <?php get_Bookings(); ?>
+        </tbody>
+    </table>
+</div>
--- learningspacefinal/AccSystem/resource/templates/back/viewing.php
+++ learningspacefinal/AccSystem/resource/templates/back/viewing.php
@@ -1,22 +1,48 @@
-
-    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Viewings</h1>
-        <div class="btn-toolbar mb-2 mb-md-0">
-            Some
-<!--            <div class="btn-group mr-2">
-                <button class="btn btn-sm btn-outline-secondary">Share</button>
-                <button class="btn btn-sm btn-outline-secondary">Export</button>
-            </div>
-            <button class="btn btn-sm btn-outline-secondary dropdown-toggle">
-                <span data-feather="calendar"></span>
-                This week
-            </button>-->
-        </div>
+     
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="page-header">Viewings</h1>
+    <div class="btn-toolbar mb-2 mb-md-0">
+        <?php if(isset($_GET['student'])){ 
+            if($_GET['student']=="deleted"){ ?>
+        <div class="alert alert-success alert-dismissible fade show text-center" role="alert">
+                <strong>Success!</strong> A student record was removed.
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button></div>
+            <?php }}?>
+        
     </div>
+</div>
 
-    <!--<canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>-->
-
-    <h2>Section title</h2>
-    <div class="table-responsive">
-
+<div class="row">
+    <div class="col-lg-12">
+        <ol class="breadcrumb text-secondary ">
+            <li class="breadcrumb-item text-secondary"><i class="fa fa-book-open"></i> Viewings</li>  
+        </ol>
     </div>
+</div>
+<!--</nav>-->
+
+<div class="table-responsive">
+    <table class="table table-striped table-sm">
+        <thead>
+            <tr>  
+                <th>ID</th>
+                <th>viewer_Name</th>
+                <th>viewer_Email</th>
+                <th>viewer_Phone</th>
+                <th>view_Date</th>
+                <th>view_Status</th>
+                <th>Room_Name</th>
+                <th>Date</th>
+
+                <th>Pending</th>
+                <th>Edit</th>
+                <th>Remove</th>
+            </tr>
+        </thead>
+        <tbody class="text-center">
+            <?php get_Viewings(); ?>
+        </tbody>
+    </table>
+</div>
