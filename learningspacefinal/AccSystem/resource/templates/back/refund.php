94ee6b64d1f46ae6525236b018dc74bd3dd6fe86
Robert Berge <31069197+ZenitH-AT@users.noreply.github.com>
2018-09-30 05:02:48 +0200

    Payment refunding

--- learningspacefinal/AccSystem/resource/templates/back/refund.php
+++ learningspacefinal/AccSystem/resource/templates/back/refund.php
@@ -1,146 +1,61 @@
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h2">Refund requests</h1>
+</div>
 
-    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Refund Requests</h1>
-        <div class="btn-toolbar mb-2 mb-md-0">
-            Some
-<!--            <div class="btn-group mr-2">
-                <button class="btn btn-sm btn-outline-secondary">Share</button>
-                <button class="btn btn-sm btn-outline-secondary">Export</button>
-            </div>
-            <button class="btn btn-sm btn-outline-secondary dropdown-toggle">
-                <span data-feather="calendar"></span>
-                This week
-            </button>-->
-        </div>
-    </div>
-
-    <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
-
-    <h2>Section title</h2>
+<h5>Request records</h5>
+<div class="table-responsive">
     <div class="table-responsive">
         <table class="table table-striped table-sm">
             <thead>
                 <tr>
-                    <th>#</th>
-                    <th>Header</th>
-                    <th>Header</th>
-                    <th>Header</th>
-                    <th>Header</th>
+                    <th>Request ID</th>
+                    <th>Payment ID<br />(payment to be revoked)</th>
+                    <th>Student ID</th>
+                    <th>Reason</th>
+                    <th>Date requested</th>
+
+                    <th>Accept</th>
+                    <th>Decline</th>
                 </tr>
             </thead>
-            <tbody>
-                <tr>
-                    <td>1,001</td>
-                    <td>Lorem</td>
-                    <td>ipsum</td>
-                    <td>dolor</td>
-                    <td>sit</td>
-                </tr>
-                <tr>
-                    <td>1,002</td>
-                    <td>amet</td>
-                    <td>consectetur</td>
-                    <td>adipiscing</td>
-                    <td>elit</td>
-                </tr>
-                <tr>
-                    <td>1,003</td>
-                    <td>Integer</td>
-                    <td>nec</td>
-                    <td>odio</td>
-                    <td>Praesent</td>
-                </tr>
-                <tr>
-                    <td>1,003</td>
-                    <td>libero</td>
-                    <td>Sed</td>
-                    <td>cursus</td>
-                    <td>ante</td>
-                </tr>
-                <tr>
-                    <td>1,004</td>
-                    <td>dapibus</td>
-                    <td>diam</td>
-                    <td>Sed</td>
-                    <td>nisi</td>
-                </tr>
-                <tr>
-                    <td>1,005</td>
-                    <td>Nulla</td>
-                    <td>quis</td>
-                    <td>sem</td>
-                    <td>at</td>
-                </tr>
-                <tr>
-                    <td>1,006</td>
-                    <td>nibh</td>
-                    <td>elementum</td>
-                    <td>imperdiet</td>
-                    <td>Duis</td>
-                </tr>
-                <tr>
-                    <td>1,007</td>
-                    <td>sagittis</td>
-                    <td>ipsum</td>
-                    <td>Praesent</td>
-                    <td>mauris</td>
-                </tr>
-                <tr>
-                    <td>1,008</td>
-                    <td>Fusce</td>
-                    <td>nec</td>
-                    <td>tellus</td>
-                    <td>sed</td>
-                </tr>
-                <tr>
-                    <td>1,009</td>
-                    <td>augue</td>
-                    <td>semper</td>
-                    <td>porta</td>
-                    <td>Mauris</td>
-                </tr>
-                <tr>
-                    <td>1,010</td>
-                    <td>massa</td>
-                    <td>Vestibulum</td>
-                    <td>lacinia</td>
-                    <td>arcu</td>
-                </tr>
-                <tr>
-                    <td>1,011</td>
-                    <td>eget</td>
-                    <td>nulla</td>
-                    <td>Class</td>
-                    <td>aptent</td>
-                </tr>
-                <tr>
-                    <td>1,012</td>
-                    <td>taciti</td>
-                    <td>sociosqu</td>
-                    <td>ad</td>
-                    <td>litora</td>
-                </tr>
-                <tr>
-                    <td>1,013</td>
-                    <td>torquent</td>
-                    <td>per</td>
-                    <td>conubia</td>
-                    <td>nostra</td>
-                </tr>
-                <tr>
-                    <td>1,014</td>
-                    <td>per</td>
-                    <td>inceptos</td>
-                    <td>himenaeos</td>
-                    <td>Curabitur</td>
-                </tr>
-                <tr>
-                    <td>1,015</td>
-                    <td>sodales</td>
-                    <td>ligula</td>
-                    <td>in</td>
-                    <td>libero</td>
-                </tr>
+            <tbody> <?php
+                $query = query("SELECT * FROM refund ORDER BY requestID DESC");
+                confirm($query);
+    
+                while ($row = fetch_array($query)) { ?>
+                    <tr>
+                        <td><?php echo $row['requestID'] ?></td>
+                        <td><?php echo $row['payID'] ?></td>
+                        <td><?php echo $row['studID'] ?></td>
+                        <td><?php echo $row['reason'] ?></td>
+                        <td><?php echo $row['date'] ?></td>
+
+                        <td><form method="post"><button class="btn btn-success formbutton" name="accept<?php echo $row['requestID']; ?>" onclick="return confirm('Are you sure you want to accept this request?')"><span class="fa fa-check" style="color:white"></button></form></td>
+                        <td><form method="post"><button class="btn btn-danger formbutton" name="decline<?php echo $row['requestID']; ?>" onclick="return confirm('Are you sure you want to decline this request?')"><span class="fa fa-times" style="color:white"></button></form></td><?php
+
+                        //Accept button handling
+                        if(isset($_POST['accept' . $row['requestID']])){
+                            query("DELETE FROM payment WHERE payID = " . $row['payID']);
+                            ?><script>alert("The request has been accepted.");</script><?php
+
+                            send_notification("Your refund request was accepted", "Your payment refund request has been <strong>accepted</strong>.", "success", $row['studID']);
+                            
+                            header("Refresh:0");
+                            exit();
+                        }
+
+                        //Decline button handling
+                        if(isset($_POST['decline' . $row['requestID']])){
+                            query("DELETE FROM refund WHERE requestID = " . $row['requestID']);
+                            ?><script>alert("The request has been declined.");</script><?php
+
+                            send_notification("Your refund request was declined", "Your payment refund request has been <strong>declined</strong>.", "danger", $row['studID']);
+                            
+                            header("Refresh:0");
+                            exit();
+                        } ?>
+                    </tr> <?php
+                } ?>
             </tbody>
         </table>
     </div>
