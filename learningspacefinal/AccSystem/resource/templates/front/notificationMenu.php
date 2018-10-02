ec2806a69c485f673cb707141e1fdbc1507eb4eb
Robert Berge <31069197+ZenitH-AT@users.noreply.github.com>
2018-10-02 02:17:27 +0200

    Limit height of dropdown and add scrollbar

--- learningspacefinal/AccSystem/resource/templates/front/notificationMenu.php
+++ learningspacefinal/AccSystem/resource/templates/front/notificationMenu.php
@@ -20,30 +20,30 @@
                     <span>Notifications</span> <?php echo '(' .  $unreadcount . ')'; ?>
                 </div>
             </div>
-        </li><?php
-
-        //Generating notification cards
-        if($unreadcount > 0) {
-            while ($row = fetch_array($sqlunreaddropdown)) { ?>
-                <li class="notification-box alert-message-<?php echo $row['type'] ?>">
-                    <div class="row">
-                        <div class="col-lg-1 col-sm-1 col-1 text-center"></div>
-                        <div class="col-lg-10 col-sm-10 col-10">
-                            <strong class="text-dark"><?php echo $row['title'] ?></strong>
-                            <div style="white-space:nowrap; overflow:hidden; text-overflow:ellipsis;">
-                                <?php echo $row['body'] ?>
+        </li>
+        <div style="height:300px; overflow-y:scroll; overflow-x:hidden;"><?php
+            //Generating notification cards
+            if($unreadcount > 0) {
+                while ($row = fetch_array($sqlunreaddropdown)) { ?>
+                    <li class="notification-box alert-message-<?php echo $row['type'] ?>">
+                        <div class="row">
+                            <div class="col-lg-1 col-sm-1 col-1 text-center"></div>
+                            <div class="col-lg-10 col-sm-10 col-10">
+                                <strong class="text-dark"><?php echo $row['title'] ?></strong>
+                                <div style="white-space:nowrap; overflow:hidden; text-overflow:ellipsis;">
+                                    <?php echo $row['body'] ?>
+                                </div>
+                                <small class="text-muted"><?php echo $row['time'] ?></small>
                             </div>
-                            <small class="text-muted"><?php echo $row['time'] ?></small>
                         </div>
-                    </div>
-                </li><?php
-            } 
-        } else { ?>
-            <div class='notification-box alert-secondary text-center'>
-                <strong>All caught up!</strong>
-            </div><?php 
-        } ?>
-
+                    </li><?php
+                } 
+            } else { ?>
+                <div class='notification-box alert-secondary text-center'>
+                    <strong>All caught up!</strong>
+                </div><?php 
+            } ?>
+        </div>
         <li class="footer bg-dark text-center">
             <a href="userNotification.php" class="text-light">View All</a>
         </li>
