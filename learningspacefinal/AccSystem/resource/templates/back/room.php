d07229c053a4ee9c16f3ee23443b1873a79b311e
Robert Berge <31069197+ZenitH-AT@users.noreply.github.com>
2018-10-03 03:18:27 +0200

    Add files via upload

--- learningspacefinal/AccSystem/resource/templates/back/room.php
+++ learningspacefinal/AccSystem/resource/templates/back/room.php
@@ -1,3 +1,7 @@
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h2">Rooms</h1>
+</div>
+
 <div class="row">
     <div class="col-lg-12">
         <ol class="breadcrumb text-secondary ">
