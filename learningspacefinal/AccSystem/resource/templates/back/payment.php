f63882cf85ed41ecf4e83b849a65fe4a3d1322a3
Robert Berge <31069197+ZenitH-AT@users.noreply.github.com>
2018-10-08 21:04:28 +0200

    Added switch status button

--- learningspacefinal/AccSystem/resource/templates/back/payment.php
+++ learningspacefinal/AccSystem/resource/templates/back/payment.php
@@ -16,7 +16,8 @@
                     <th>Payment month</th>
                     <th>Student ID</th>
                     <th>Room ID</th>
-                    <th>Payment status</th>
+                    <th>Status</th>
+                    <th></th>
                     <th>Payment date</th>
                 </tr>
             </thead>
@@ -34,9 +35,20 @@
                         <td><?php echo $row['payMonth'] ?></td>
                         <td><?php echo $row['studID'] ?></td>
                         <td><?php echo $row['roomID'] ?></td>
-                        <td><?php echo ($row['paymentStatus'] == 1 ? '<text class="text-success">paid</text>' : '<text class="text-danger">unpaid</text>') ?></td>
+                        <td><?php echo ($row['paymentStatus'] == 1 ? '<text class="text-success" style="float:left">paid</text>' : '<text class="text-danger" style="float:left">unpaid</text>') ?></td>
+                        <td><form method="post"><button class="btn-xs btn-dark formbutton" name="switch<?php echo $row['payID']; ?>" onclick="return confirm('Are you sure you want to switch the payment status of payment ID <?php echo $row['payID'] ?>?')"><span class="fas fa-exchange-alt" style="color:white"></button></form></td>
                         <td><?php echo $row['paymentDate'] ?></td>
                     </tr> <?php
+
+                    //Switch button handling
+                    if(isset($_POST['switch' . $row['payID']])) {
+                        $newValue = ($row['paymentStatus'] == 1 ? 0 : 1);
+
+                        query("UPDATE payment SET paymentStatus = {$newValue} WHERE payID = " . $row['payID']);
+
+                        header("Refresh:0");
+                        exit();
+                    }
                 } ?>
             </tbody>
         </table>
