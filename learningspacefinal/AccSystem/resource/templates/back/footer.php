4c14f20b22e687755a986797c87c9a3eb3291c08
Robert Berge <31069197+ZenitH-AT@users.noreply.github.com>
2018-10-16 16:21:26 +0200

    Reworded account error messages

---
+++ learningspacefinal/AccSystem/resource/templates/back/footer.php
@@ -0,0 +1,131 @@
+<!-- live chat float include -->
+<!-- is run on a node.js server -->
+<!-- note: 'allow_url_include = 1' must be added to php.ini for this to work-->
+<?php
+    if (isset($_SESSION["iduser"])) {
+        @include 'http://localhost:8088/client.php';
+        ?>
+            <script>socket.connect('http://localhost:8088');</script>
+        <?php
+    } 
+?>
+
+<!--Alert Message For Login *****************************************-->
+<!--error=1 is used to display alert msg of wrong pass or email-->
+<!--error=2 -->
+<?php
+if (isset($_GET['error'])) {
+    $error = $_GET['error'];
+    if ($error == 1) {
+        ?>
+        <script>alert("Your credentials are incorrect. Please try signing in again with the correct information.");</script>
+        <?php
+    }
+    if ($error == 2) {
+        ?>
+        <script>alert("Your email is incorrect. Please try again using the correct email.");</script>
+        <?php
+    }
+    if ($error == 3) {
+        ?>
+        <script>alert("A reset password link was sent to your email.");</script>
+        <?php
+    }
+    if ($error == 4) {
+        ?>
+        <script>alert("Sign in fields cannot be empty. Please try again.");</script>
+        <?php
+    }
+    if ($error == 5) {
+        ?>
+        <script>alert("Your account has not yet been activated. Please check your email.");</script>
+        <?php
+    }
+    if ($error == 6) {
+        ?>
+        <script>alert("A password reset link was sent to your email");</script>
+        <?php
+    }
+    if ($error == 7) {
+        ?>
+        <script>alert("Internet connection error or the provided email was invalid.");</script>
+        <?php
+    }
+    if ($error == 8) {
+        ?>
+        <script>alert("Your password has been changed. Please sign in again using the new password.");</script>
+        <?php
+    }
+}
+?>
+
+<footer class="container text-center">
+    <p>&copy; 2017-2018 DreamTeam, Inc. &middot; <a href="PrivacyTerms.php" data-toggle="modal" data-target="#PrivacyPopup">Privacy</a> &middot; <a href="PrivacyTerms.php" data-toggle="modal" data-target="#TermsPopup">Terms</a></p>
+</footer>
+
+<?php include "PrivacyTerms.php"; ?>
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="./js/jquery-3.3.1.slim.min.js"></script>
+<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+<script src="./js/popper.min.js"></script>
+<script src="./js/bootstrap.min.js"></script>
+<script src="js/main.js"></script>
+
+<!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+<script src="./js/holder.min.js"></script>
+<script src="./js/float-panel.js"></script>
+<script src="./js/jquery.datetimepicker.full.js"></script>
+
+<!-- for gallery images -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.7.2/angular.min.js"></script>
+
+<script>baguetteBox.run('.tz-gallery');</script>
+
+<script>
+    //Function for ToolTip used in BookingPage
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+</script>
+
+<script>
+    $(document).ready(function() {
+        $("#bookviewings").datetimepicker({
+            minDate: '+2',
+            allowTimes: ['12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00'],
+            beforeShowDay:
+                    function(date) {
+                        return [date.getDay() == 0 ? false : true];
+                        //return [date.getDay() == 5 || date.getDay() == 0 ? false : true];// Restric Saturdays and Sundays
+                    }
+        });
+        
+        $("#checkin").datetimepicker({
+            minDate: '+2',
+            formatTime:	false,
+            format:	'Y/m/d',
+            //allowTimes: ['12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00'],
+            beforeShowDay:
+                    function(date) {
+                        return [date.getDay() == 0 ? false : true];
+                        //return [date.getDay() == 5 || date.getDay() == 0 ? false : true];// Restric Saturdays and Sundays
+                    }
+        });
+        
+        $("#checkout").datetimepicker({
+            minDate: '+2',
+            formatTime:	false,
+            format:	'Y/m/d',
+            beforeShowDay:
+                    function(date) {
+                        return [date.getDay() == 0 ? false : true];
+                    }
+        });
+        
+    });
+</script>
