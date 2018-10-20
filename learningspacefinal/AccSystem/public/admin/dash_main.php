1fcaeca570f671785cc28afb3d701da05305c5b0
Wenykeny <31759520+Wenykeny@users.noreply.github.com>
2018-10-20 15:35:24 +0200

    Changes were made
    I HAVE CHANGED THESE FILES FOR ADMIN DASHBOARD

--- learningspacefinal/AccSystem/public/admin/dash_main.php
+++ learningspacefinal/AccSystem/public/admin/dash_main.php
@@ -1,18 +1,17 @@
-
+<?php require_once './js/chart.php'; ?>
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="page-header">Dashboard <small>Statistics Overview</small></h1>
+    <h1 class="page-header">Dashboard <small>Overview</small></h1>
     <div class="btn-toolbar mb-2 mb-md-0">
         <!-- some text here -->
     </div>
 
 </div>
 
-<div class="row">
-    <div class="col-lg-12">
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item text-secondary"><i class="fa fa-tachometer-alt"></i> Dashboard</li>
-        </ol>
-    </div>
-</div>
-
-<canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
+<?php require_once './overviews/studentsOverview.php'; ?>
+<?php require_once './overviews/viewingsOverview.php'; ?>
+<?php require_once './overviews/bookingsOverview.php'; ?>
+<?php require_once './overviews/roomsOverview.php'; ?>
+<?php require_once './overviews/notificationOverview.php'; ?>
+<?php require_once './overviews/ticketOverview.php'; ?>
+<?php require_once './overviews/adminOverview.php'; ?>
+<?php require_once './overviews/graphsOverview.php'; ?>
