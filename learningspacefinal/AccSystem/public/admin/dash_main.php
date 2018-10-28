e734793bdb7a84e7c7dd86c6fcdca3fbdd5dbb28
Wenykeny <31759520+Wenykeny@users.noreply.github.com>
2018-10-28 23:08:20 +0200

    CAHNGED A BIT

--- learningspacefinal/AccSystem/public/admin/dash_main.php
+++ learningspacefinal/AccSystem/public/admin/dash_main.php
@@ -1,17 +1,9 @@
 <?php require_once './js/chart.php'; ?>
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="page-header">Dashboard <small>Overview</small></h1>
+    <h1 class="page-header">Dashboard </h1>
     <div class="btn-toolbar mb-2 mb-md-0">
         <!-- some text here -->
     </div>
 
 </div>
-
-<?php require_once './overviews/studentsOverview.php'; ?>
-<?php require_once './overviews/viewingsOverview.php'; ?>
-<?php require_once './overviews/bookingsOverview.php'; ?>
-<?php require_once './overviews/roomsOverview.php'; ?>
-<?php require_once './overviews/notificationOverview.php'; ?>
-<?php require_once './overviews/ticketOverview.php'; ?>
-<?php require_once './overviews/adminOverview.php'; ?>
-<?php require_once './overviews/graphsOverview.php'; ?>
+<?php require_once './overviews/FirstRowOverview.php'; ?>
