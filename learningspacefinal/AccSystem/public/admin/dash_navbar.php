87a0b27039d8e897e30020fbfdc1d1a2a0e6ef35
Robert Berge <31069197+ZenitH-AT@users.noreply.github.com>
2018-10-30 00:54:12 +0200

    fixed design

--- learningspacefinal/AccSystem/public/admin/dash_navbar.php
+++ learningspacefinal/AccSystem/public/admin/dash_navbar.php
@@ -4,8 +4,8 @@
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarCollapse">
-        <ul class="navbar-nav mr-auto" style="width:45%;">
-            <input class="form-control form-control-dark w-100" id="searchFilter" style="margin-left: 1%;" placeholder="Filter table" aria-label="Search" type="text">  
+        <ul class="navbar-nav mr-auto" style="width: 46%; margin-left: 2%;">
+            <input class="form-control form-control-dark w-100" id="searchFilter" placeholder="Filter table" aria-label="Search" type="text">  
         </ul>
         <ul class="navbar-nav">
             <li class="nav-item text-nowrap" style="padding-right: 11px;">
@@ -13,7 +13,7 @@
             </li>
         </ul>
         <div style="padding-right: 15px;">
-            <a href="../HomePage.php"><button type="button" class = "btn btn-outline-success my-2 my-sm-0" style="padding-right: 6px;"><i class="fa fa-fw fa-home"></i> Home Page</button></a>
+            <a href="../HomePage.php"><button type="button" class="btn btn-outline-success my-2 my-sm-0" style="padding-right: 6px;"><i class="fa fa-fw fa-home"></i> Home Page</button></a>
             <a href="../../resource/logout.php"><button type="button" class = "btn btn-outline-success my-2 my-sm-0"><i class="fa fa-fw fa-power-off"></i> Sign out</button></a>
         </div>
     </div>
