4be090496b177dfe5ef6625231465cc6eecc7084
Robert Berge <31069197+ZenitH-AT@users.noreply.github.com>
2018-11-01 01:22:03 +0200

    Update FirstRowOverview.php

--- learningspacefinal/AccSystem/public/admin/overviews/FirstRowOverview.php
+++ learningspacefinal/AccSystem/public/admin/overviews/FirstRowOverview.php
@@ -1,28 +1,28 @@
 <div id="accordionALL">
     <div class="row">
         <div class="col-lg-3" id="headingStudent">
-            <a href="#" class="font-weight-bold" style="text-decoration: none;" data-toggle="collapse" data-target="#collapseStudent">
+            <a href="#" class="font-weight-bold" style="text-decoration: none;" data-toggle="collapse" data-target="#collapseStudent" data-parent="#accordionALL">
                 <ol class="breadcrumb">
                     <li class="breadcrumb-item text-secondary" ><i class="fa fa-user-graduate"></i> Student Overviews</li>
                 </ol>
             </a>
         </div>
         <div class="col-lg-3" id="headingViewing">
-            <a href="#" class="font-weight-bold" style="text-decoration: none;" data-toggle="collapse" data-target="#collapseViewing">
+            <a href="#" class="font-weight-bold" style="text-decoration: none;" data-toggle="collapse" data-target="#collapseViewing" data-parent="#accordionALL">
                 <ol class="breadcrumb">
                     <li class="breadcrumb-item text-secondary" ><i class="fa fa-eye"></i> Viewing Overviews</li>
                 </ol>
             </a>
         </div>
         <div class="col-lg-3">
-            <a href="#" class="font-weight-bold" style="text-decoration: none;" data-toggle="collapse" data-target="#collapsebooking" >
+            <a href="#" class="font-weight-bold" style="text-decoration: none;" data-toggle="collapse" data-target="#collapsebooking" data-parent="#accordionALL">
                 <ol class="breadcrumb">
                     <li class="breadcrumb-item text-secondary" ><i class="fa fa-book-open"></i> Booking Overviews</li>
                 </ol>
             </a>
         </div>
         <div class="col-lg-3">
-            <a href="#" class="font-weight-bold" style="text-decoration: none;" data-toggle="collapse" data-target="#roomOverview" >
+            <a href="#" class="font-weight-bold" style="text-decoration: none;" data-toggle="collapse" data-target="#roomOverview" data-parent="#accordionALL">
                 <ol class="breadcrumb">
                     <li class="breadcrumb-item text-secondary" ><i class="fa fa-home"></i> Room Overviews</li>
                 </ol>
@@ -32,28 +32,28 @@
     
     <div class="row">
     <div class="col-lg-3" id="headingNote">
-        <a href="#" class="font-weight-bold" style="text-decoration: none;" data-toggle="collapse" data-target="#noteOverview" >
+        <a href="#" class="font-weight-bold" style="text-decoration: none;" data-toggle="collapse" data-target="#noteOverview" data-parent="#accordionALL">
             <ol class="breadcrumb">
                 <li class="breadcrumb-item text-secondary" ><i class="fa fa-bell"></i> Notification Overviews</li>
             </ol>
         </a>
     </div>
     <div class="col-lg-3" id="headingticket">
-        <a href="#" class="font-weight-bold" style="text-decoration: none;" data-toggle="collapse" data-target="#ticketOverview" >
+        <a href="#" class="font-weight-bold" style="text-decoration: none;" data-toggle="collapse" data-target="#ticketOverview" data-parent="#accordionALL">
             <ol class="breadcrumb">
                 <li class="breadcrumb-item text-secondary" ><i class="fa fa-life-ring"></i> Ticket Overviews</li>
             </ol>
         </a>
     </div>
     <div class="col-lg-3">
-        <a href="#" class="font-weight-bold" style="text-decoration: none;" data-toggle="collapse" data-target="#adminOverview" >
+        <a href="#" class="font-weight-bold" style="text-decoration: none;" data-toggle="collapse" data-target="#adminOverview" data-parent="#accordionALL">
             <ol class="breadcrumb">
                 <li class="breadcrumb-item text-secondary" ><i class="fa fa-users-cog"></i> Administrator Overviews</li>
             </ol>
         </a>
     </div>
     <div class="col-lg-3">
-        <a href="#" class="font-weight-bold" style="text-decoration: none;" data-toggle="collapse" data-target="#graphOverview" >
+        <a href="#" class="font-weight-bold" style="text-decoration: none;" data-toggle="collapse" data-target="#graphOverview" data-parent="#accordionALL">
             <ol class="breadcrumb">
                 <li class="breadcrumb-item text-secondary" ><i class="fa fa-chart-bar"></i> Graph Overviews</li>
             </ol>
